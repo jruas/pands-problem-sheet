f8cc586793e306e40978bc35cb7d217bd2e77459
unknown <g00398265@gmit.ie>
2021-03-29 15:04:04 +0100

    Update of Week 2

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-# pands-problem-sheet
+# pands-problem-sheet : JOANA RUAS
+----------------------------------
+
+
+# Week 1:
+    Setting up environment
+    Installing git hub
+    Linking pands-problem-sheet to git hub
+
+# Week2: 
+Calculation of Body Mass Index (BMI), getting weight and height from user.
+file: bmi.py
+key-sections of code:
+    .Take in height and weight from user
+    .Converstion of input values to float
+    .Converstion of height to metres
+    .BMI calculation
+    .Printing result to user with 2 decimal places in a string "BMI is XX.XX"
+    
--- bmi.py
+++ bmi.py
@@ -2,11 +2,16 @@
 # Author: Joana Ruas
 
 # The program calculates the Body Maxx Index (BMI)
+# Program takes from user weight (kg) and height (cm)
+# Outputs the BMI in weight by m2
 
-height = float(input ('Please insert your height:'))
-weight = float(input ('Please insert your weight:'))
-newheight = (height/100)
-bmi = (weight/newheight**2)
+
+height = float(input ('Please insert your height in centimetres:'))     #Converstion of input to Float, as input is a string by default.
+weight = float(input ('Please insert your weight in Kilograms:'))
+
+
+newheight = (height/100)                                                #Converstion of height to metres.  
+bmi = (weight/newheight**2)                                             #bmi calculation
 
 
 print ('BMI is '+ str(round(bmi,2))+".")
